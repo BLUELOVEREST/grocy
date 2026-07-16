eb01a5191cb8056ac78da5b945c13a0914307d4a
zhangzhicheng <zhangzhicheng@users.noreply.github.com>
2026-07-16 09:05:27 +0000

    Revert "feat: add apple style grocy theme"
    This reverts commit accd279c2ae85495fd05a7b133f2eb1d7bc4d3e4.

--- views/layout/default.blade.php
+++ views/layout/default.blade.php
@@ -91,9 +91,6 @@
 
 	@stack('pageStyles')
 
-	<link href="{{ $U('/css/eric_apple_theme.css?v=', true) }}{{ $version }}"
-		rel="stylesheet">
-
 	@if(file_exists(GROCY_DATAPATH . '/custom_css.html'))
 	@php include GROCY_DATAPATH . '/custom_css.html' @endphp
 	@endif

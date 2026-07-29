12a3a786bd05bd99ebf279ec5f2e33b4de2c50f4
zhangzhicheng <zhangzhicheng@users.noreply.github.com>
2026-07-29 06:09:30 +0000

    feat: add product nutrition table

---
+++ migrations/0259.sql
@@ -0,0 +1,18 @@
+CREATE TABLE product_nutrition (
+	product_id INTEGER NOT NULL PRIMARY KEY,
+	basis_amount REAL NOT NULL DEFAULT 100,
+	basis_qu_id INTEGER NOT NULL,
+	calories REAL,
+	protein REAL,
+	fat REAL,
+	carbohydrates REAL,
+	row_created_timestamp DATETIME DEFAULT (datetime('now', 'localtime')),
+	FOREIGN KEY(product_id) REFERENCES products(id),
+	FOREIGN KEY(basis_qu_id) REFERENCES quantity_units(id)
+);
+
+CREATE TRIGGER cascade_product_nutrition_removal AFTER DELETE ON products
+BEGIN
+	DELETE FROM product_nutrition
+	WHERE product_id = OLD.id;
+END;
